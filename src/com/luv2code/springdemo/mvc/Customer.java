0535d300825b10f8c3f7b762b2e905400bc8d7ed
Jupraj Singh <juprajsingh1@gmail.com>
2019-06-15 01:39:56 -0400

    Made an integer field required, check freePasses inside Customer class

--- src/com/luv2code/springdemo/mvc/Customer.java
+++ src/com/luv2code/springdemo/mvc/Customer.java
@@ -11,18 +11,20 @@ public class Customer {
 	
 	private String firstName;
 	
-	
-	
 	@NotNull(message = "is required")
 	@Size(min=1, message="is required")
 	private String lastName;
 	
+	@NotNull(message = "is required")
 	@Min(value=0, message= "must be greater than or equal to zero")
 	@Max(value=10, message= "must be less than or equal to 10")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
+	
+
+	
 	public String getPostalCode() {
 		return postalCode;
 	}
@@ -45,10 +47,10 @@ public class Customer {
 	
 	
 	
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 	
